29b132465b79bf12ec5bc6b615183305e512279d
Agboola Mukhtar <mukhtaragboola@gmail.com>
2019-10-06 18:24:55 +0100

    Sentiment Analysis on Twitter Bot

---
+++ twitter_credentials.py
@@ -0,0 +1,5 @@
+# Variables that contain the user credentials to access Twitter API
+ACCESS_TOKEN = "965215367860809728-nCUC1u58RG8A9UwGDsF6z0Ts86fpCfl"
+ACCESS_TOKEN_SECRET = "SxwAvywjoyfdrfxxiLkd2DBLsDzsGeFYpG0irZz3eCi1n"
+CONSUMER_KEY = "B8iUI7VPsyX3C6VXz4br08BxL"
+CONSUMER_SECRET = "aEbKvfOubAbCEfmsh8KL7LW4VHDbkqoT2GSplMjoIpyTjl3qF8"
